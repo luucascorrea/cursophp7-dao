8c4b2c6ba5f4d894caa70306db0ca00c2171584f
Lucas Correa <lucas.correa1914@hotmail.com>
2020-05-29 19:34:40 -0300

    metodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -55,6 +55,41 @@ class Usuario {
 
 	}
 
+	public static function getList(){
+
+		$sql = new Sql();
+		return $sql -> select("SELECT * FROM tb_usuario ORDER BY deslogin;");
+	}
+
+	public static function search($login){
+
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuario WHERE DESLOGIN LIKE :search order by deslogin", array(
+			':search'=>"%".$login."%"
+		));
+	}
+
+	public function login($login, $password){
+
+		$sql = new Sql();
+		$result = $sql->select("SELECT * FROM tb_usuario WHERE desusuario = :LOGIN and dessenha = :PASSWORD", array(":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if(isset($result[0])){
+			$row = $result[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		} else {
+
+
+			throw new Exception("Login e/ou senha inválidos.");
+		}
+	}
+
 
 	public function __toString(){
 
--- index.php
+++ index.php
@@ -5,11 +5,33 @@ require_once("config.php");
 //$sql = new Sql();
 //$usuarios = $sql->select("SELECT * FROM tb_usuario");
 
-$root = new Usuario();
-$root->loadById(3);
+//$root = new Usuario();
+//$root->loadById(3);
 
-echo $root;
+//echo $root;
 
 //echo json_encode($usuarios)
+
+//Carrega lista de usuários
+
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+
+
+//Carrega uma lista de usuario buscando pelo login
+
+//$search =Usuario::search("jo");
+//echo json_encode($search);
+
+
+//Carrega um usuário usando login e senha
+
+$usuario = new Usuario();
+$usuario->login("root","123");
+
+echo $usuario;
+
 
 ?>
